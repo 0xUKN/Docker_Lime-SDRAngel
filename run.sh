8b73e93a85544e1d325fd2a0a4a07265c850c132
0xUKN <38835896+0xUKN@users.noreply.github.com>
2020-02-03 23:38:21 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,3 +2,4 @@
 
 xhost +
 docker run --net=host --env="DISPLAY" --env="PULSE_SERVER=unix:/run/user/1000/pulse/native" --volume="$HOME/.Xauthority:/root/.Xauthority:rw" --volume="/dev/bus/usb:/dev/bus/usb:rw" --volume="/run/user/1000/pulse:/run/user/1000/pulse:rw" --privileged lime-sdrangel:latest
+xhost -
